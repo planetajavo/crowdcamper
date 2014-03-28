96e6e546d080b7aa9ab778d894215455b9131c01
planetajavo <planetajavo@yahoo.es>
2014-03-28 09:27:32 +0000

    Unavailability, Rental, Downtime, User models & Devise installation

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,8 +1,9 @@
 class Van < ActiveRecord::Base
 	has_many :locations
 	has_many :images
+	has_many :unavailabilities
 
-	mount_uploader :avatar, AvatarUploader
+	mount_uploader :image, ImageUploader
 
-	
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Crowdcamper::Application.routes.draw do
 
+  devise_for :users
   resources :vans
 
   root 'sites#home'
